5c2c3efe1da9337365cad99718aa0207ebea8149
Spacemanx3 <127046307+Spacemanx3@users.noreply.github.com>
2023-12-13 18:17:19 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,18 +1,12 @@
-from flask import Flask, redirect, url_for, request
-app = Flask(__name__)
+from flask import Flask
+app = Flask('app')
 
-@app.route('/success/<name>')
-def success(name):
-   return 'welcome %s' % name
+@app.route('/hello')
+def hello_world():
+  return 'Hello, World!'
+  
+@app.route('/admin')
+def admin_page():
+  return 'Hello Admin'
 
-@app.route('/login',methods = ['POST', 'GET'])
-def login():
-   if request.method == 'POST':
-      user = request.form['nm']
-      return redirect(url_for('success',name = user))
-   else:
-      user = request.args.get('nm')
-      return redirect(url_for('success',name = user))
-
-if __name__ == '__main__':
-   app.run(debug = True)
+app.run(host='0.0.0.0', port=8080)
