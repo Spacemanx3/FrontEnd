291ce72dc5a7b3cc8df37e22a5cf19a787f240b4
Spacemanx3 <127046307+Spacemanx3@users.noreply.github.com>
2023-12-13 21:34:52 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,27 +1,20 @@
 from flask import Flask, redirect, url_for
-app = Flask('app')
+app = Flask(__name__)
 
-@app.route('/hello')
-def hello_world():
-  return 'Hello, World!'
-  
+@app.route('/admin')
+def hello_admin():
+   return 'Hello Admin'
+
 @app.route('/guest/<guest>')
 def hello_guest(guest):
-  return 'Hello, %s!' % guest
-  
-@app.route('/user/<username>')
-def show_user_profile(username):
-  # show the user profile for that user
-  return 'User %s' % username
-  
-@app.route('/post/<int:post_id>')
-def show_post(post_id):
-  # show the post with the given id, the id is an integer
-  return 'Post %d' % post_id
-  
-@app.route('/admin')
-def admin_page():
-  return 'Hello Admin'
+   return 'Hello %s as Guest' % guest
 
+@app.route('/user/<name>')
+def hello_user(name):
+   if name =='admin':
+      return redirect(url_for('hello_admin'))
+   else:
+      return redirect(url_for('hello_guest',guest = name))
 
-app.run(host='0.0.0.0', port=8080)
+if __name__ == '__main__':
+   app.run(debug = True)
