43f67a1432caa14323883a1108852282d1cd38d5
Spacemanx3 <127046307+Spacemanx3@users.noreply.github.com>
2023-12-24 20:57:46 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,9 +1,14 @@
 from flask import Flask
-app  = Flask(__name__)
+
+app = Flask(__name__)
+
+# Prompt the user for the host and port to run the server
+host = input("Enter the host (default is 127.0.0.1): ") or '127.0.0.1'
+port = input("Enter the port (default is 5000): ") or 5000
 
 @app.route('/')
-def index():
-    return '<html><body><h1>Hello World!</h1></body></html>'
-  
+def hello_world():
+    return 'Hello, World! This is a basic Flask application.'
+
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=8080)
+    app.run(host=host, port=port, debug=True)
