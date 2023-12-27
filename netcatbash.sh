557535a2280013854f3d3483dd43aaaa8aa336cb
Spacemanx3 <127046307+Spacemanx3@users.noreply.github.com>
2023-12-27 01:04:40 -0500

    Update netcatbash.sh

--- netcatbash.sh
+++ netcatbash.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Check if port 8000 is open on localhost (127.0.0.1)
-if nc -zv 127.0.0.1 8000; then
+if nc -zv 0.0.0.0 8000; then
     echo "Port 8000 is open. Displaying 'Hello, World2!' on the web page."
     echo -e "HTTP/1.1 200 OK\r\nContent-Length: $(echo -n 'Hello, World2!' | wc -c)\r\n\r\nHello, World2!"
 else
